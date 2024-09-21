3617b4ebbb48bacdd9f509f58bf5358c4ac74e25
Victor Wanderley Barbosa <61347148+vwnd@users.noreply.github.com>
2024-09-21 18:50:01 +0200

    feat: theming

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,14 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import { ThemeProvider } from "@/components/theme-provider";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
   weight: "100 900",
 });
+
 const geistMono = localFont({
   src: "./fonts/GeistMonoVF.woff",
   variable: "--font-geist-mono",
@@ -24,11 +26,18 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="light"
+          enableSystem={false}
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,16 @@
-import { Button } from "@/components/ui/button";
+import { ModeToggle } from "@/components/mode-toggle";
+import { StickyNote } from "lucide-react";
 
 export default function Home() {
   return (
-    <div>
-      <Button>Click me</Button>
+    <div className="p-3 border-b">
+      <div className="flex justify-between max-w-7xl mx-auto">
+        <div className="flex items-center gap-2">
+          <StickyNote className="text-primary" />
+          <span className="font-bold text-2xl text-primary">ark</span>
+        </div>
+        <ModeToggle />
+      </div>
     </div>
   );
 }
